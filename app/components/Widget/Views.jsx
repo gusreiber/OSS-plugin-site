f7129b96cf39c21e5631177be7e2057ebf2baeaf
gusreiber <greiber@cloudbees.com>
2016-08-04 15:30:40 -0700

    [CBN-289] fix view toggle

--- app/components/Widget/Views.jsx
+++ app/components/Widget/Views.jsx
@@ -26,21 +26,22 @@ export default class Views extends PureComponent {
 
   render() {
     const {location, router} = this.props;
-    const {view = 'Tiles'} = location.query;
+    const {view = 'Tiles'} = location.query.view;
     return (<fieldset className='btn-group'>
 
           { items.map((item, index) => {
-            const isActive = (item === location.query.view)?'active':'';
+            const isActive = (item === view )?'active':'';
             const icon = (item === 'Tiles')? 'icon-grid-alt':
               (item === 'List')? 'icon-list2' :
                 (item === 'Table')? 'icon-menu3':'';        
             return (<View key={index}
-                          icon={icon}
-                          isActive={isActive}
-                          title={item}
-                          onClick={()=> {
-             location.query.view = item;
-             router.replace(location);
+                      icon={icon}
+                      isActive={isActive}
+                      title={item}
+                      onClick={(e)=> {
+                        e.preventDefault()
+                        location.query.view = item;
+                        router.replace(location);
             }}/>);
           })}
       </fieldset>
