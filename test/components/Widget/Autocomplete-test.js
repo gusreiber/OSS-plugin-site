690bbcc354c9fae68e6f563de3d0358b28d9d4b9
Michael McCaskill <mmccaskill@cloudbees.com>
2016-09-08 15:25:29 -0400

    Clean up test

--- test/components/Widget/Autocomplete-test.js
+++ test/components/Widget/Autocomplete-test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
 import Autocomplete from '../../../app/components/Widget/Autocomplete';
 
 describe('<Autocomplete />', () => {
 
-  it("should correctly display span", () => {
+  it("should correctly display", () => {
     const props = {
       name: "autocomplete_name",
       placeholder: "autocomplete_placeholder",
@@ -14,15 +14,6 @@ describe('<Autocomplete />', () => {
     const wrapper = shallow(<Autocomplete {...props} />);
     expect(wrapper.find('span')).to.have.length(1);
     expect(wrapper.find('span').text()).to.equal(props.label);
-  });
-
-  it("should correctly display input", () => {
-    const props = {
-      name: "autocomplete_name",
-      placeholder: "autocomplete_placeholder",
-      label: "autocomplete_label"
-    };
-    const wrapper = shallow(<Autocomplete {...props} />);
     expect(wrapper.find(`input[type="text"][name="${props.name}"][placeholder="${props.placeholder}"]`)).to.have.length(1);
   });
 
