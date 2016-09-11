4779330fe230b972c783305859e7c6f846894884
Michael McCaskill <mmccaskill@cloudbees.com>
2016-09-11 13:00:50 -0400

    Clean up Categories

--- app/components/Widget/Categories.jsx
+++ app/components/Widget/Categories.jsx
@@ -1,68 +1,75 @@
 import React, { PropTypes } from 'react';
 import classNames from 'classnames';
 import styles from './Widget.css';
-import { logger } from '../../commons';
-import PureComponent from 'react-pure-render/component';
 
-export class Category extends PureComponent {
-  constructor(properties) {
-    super(properties);
-    this.state = properties.location.query
+export class Category extends React.PureComponent {
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = context.location.query;
   }
 
   static propTypes = {
-    tooltip: PropTypes.any.isRequired,
     id: PropTypes.any.isRequired,
+    labels: PropTypes.array.isRequired,
     title: PropTypes.any.isRequired,
+    tooltip: PropTypes.any.isRequired
+  };
+
+  static contextTypes = {
     location: PropTypes.object.isRequired
   };
 
   handleChange(e){
-    //TODO:FIXME: seems hacky to toss the original target and the related child labels into the event like this... 
+    //TODO:FIXME: seems hacky to toss the original target and the related child labels into the event like this...
     //but I am doing it, so hold your nose...
-    let myEvent = new Event("submit");
+    const myEvent = new Event('submit');
     myEvent.orgTarget = e.currentTarget;
     myEvent.children = this.props.labels;
     document.getElementById('plugin-search-form').dispatchEvent(myEvent);
   }
 
-  checkState(name,value){
-    const q = this.props.location.query;
+  checkState(name,value) {
+    const q = this.context.location.query;
     const qName = q[name];
     if(!q || !qName) return false;
     return qName.split(',').indexOf(value) > -1;
   }
 
   render() {
-    const { id, title, tooltip, labels, location } = this.props;
-
-    return (<li key={id} className={classNames(styles[id], id, (this.checkState('categories',id))?'mask':'')} title={tooltip}>
-      <label>
-        <input type="checkbox" name="categories" value={id}
-          checked={this.checkState('categories',id)}
-          onChange={this.handleChange.bind(this)}
-        />
-        <span>{title}</span>
-      </label>
+    const { id, title, tooltip, labels } = this.props;
+    return (
+      <li key={id} className={classNames(styles[id], id, (this.checkState('categories', id)) ? 'mask' : '')}
+        title={tooltip}
+      >
+        <label>
+          <input type="checkbox" name="categories" value={id}
+            checked={this.checkState('categories',id)}
+            onChange={this.handleChange.bind(this)}
+          />
+          <span>{title}</span>
+        </label>
         <ul>
-          {labels.map((label,index) => {
+          {labels.map((label) => {
             return(
               <li key={label.id} >
                 <label>
                   <input type="checkbox" name="labels" value={label.id} data-parent={id}
-                  checked={this.checkState('labels',label.id)}
+                  checked={this.checkState('labels', label.id)}
                   onChange={this.handleChange.bind(this)}
                 />
                   <span>{label.title}</span>
                 </label>
-              </li>);
+              </li>
+            );
           })}
         </ul>
-    </li>);
+      </li>
+    );
   }
 }
 
-export default class Categories extends PureComponent {
+export default class Categories extends React.PureComponent {
   constructor(properties) {
     super(properties);
   }
@@ -70,11 +77,11 @@ export default class Categories extends PureComponent {
   static propTypes = {
     categories: PropTypes.object.isRequired,
     labels: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired
+    handleChecks: PropTypes.func.isRequired
   };
 
   render() {
-    const {categories,labels, location, handleChecks} = this.props;
+    const {categories,labels, handleChecks} = this.props;
     const sortedCategories = categories.valueSeq();
     return (
       <fieldset className={classNames(styles.Categories)}>
@@ -85,36 +92,32 @@ export default class Categories extends PureComponent {
             value="category,labelFilter"
               onClickCapture={handleChecks.bind(this)}>Show all</button>
         </legend>
-      <ul className={classNames(styles.Cols3, "Cols3")}>
-
-      {sortedCategories.map(
-        (item, index) => {
-          if (item.id === 'junk') return null;
-          let theseLabels = [];
-          item.labels.map(labelID=>{
-            labels.map((label,i)=>{
-              if(labelID === label.id){
-                theseLabels.push(label);
-                return false;
-              }
+        <ul className={classNames(styles.Cols3, 'Cols3')}>
+          {sortedCategories.map((item) => {
+            if (item.id === 'junk') {
+              return null;
+            }
+            const theseLabels = [];
+            item.labels.map(labelID => {
+              labels.map((label) => {
+                if (labelID === label.id) {
+                  theseLabels.push(label);
+                  return false;
+                }
+              });
             });
-          });
-          return (
-            <Category
-              location={location}
-              key={item.id}
-              title={item.title}
-              tooltip={item.description}
-              id={item.id}
-              labels={theseLabels}
-            />
-          );
-        }
-      )}
-    </ul>
-    </fieldset>);
+            return (
+              <Category
+                key={item.id}
+                title={item.title}
+                tooltip={item.description}
+                id={item.id}
+                labels={theseLabels}
+              />
+            );
+          })}
+        </ul>
+      </fieldset>
+    );
   }
 }
-Categories.contextTypes = {
-    router: PropTypes.object.isRequired,
-};
