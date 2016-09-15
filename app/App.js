d147767f70b589257303fdc80516d6eafeb44e10
Michael McCaskill <mmccaskill@cloudbees.com>
2016-09-14 21:51:38 -0400

    Location no longer needed

--- app/App.js
+++ app/App.js
@@ -2,24 +2,14 @@ import React, { PropTypes } from 'react';
 
 export default class App extends React.Component {
 
-  static childContextTypes = {
-    location: PropTypes.object.isRequired
-  };
-
   static propTypes = {
     children: PropTypes.any,
   };
 
-  getChildContext() {
-    return {
-      location: this.props.location
-    };
-  }
-
   render() {
     return (
       <div>
-          {this.props.children }
+          {this.props.children}
       </div>
     );
   }
